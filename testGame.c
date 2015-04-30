7db9137847034385ce3c3acf53657d3071ecafe8
IDZ145 <IDZ145@users.noreply.github.com>
2015-04-30 22:46:28 +1000

    Update testGame.c

--- testGame.c
+++ testGame.c
@@ -1,3 +1,17 @@
 // test_game.h
 //Created by Micheal, Jospher Lauchlan and Inura
 //For COMP1917 to test whether the game.c of another group will work with our AI
+
+Written by Inura De Zoysa
+Game * knowledgeIsland;
+While(assert (knowledgeIsland =! NULL)){
+knowledgeIsland =  newGame (discipline[], dice[]);
+}
+
+action Player;
+//To test make action
+makeAction (knowledgeIsland, Player);
+
+
+disposeGame(knowledgeIsland);
+assert(knowledgeIsland == NULL);
