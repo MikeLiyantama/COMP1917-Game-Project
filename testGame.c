ceed335c2e6a110c5c4f81aa8488bdc45058bf3c
IDZ145 <IDZ145@users.noreply.github.com>
2015-04-30 23:00:51 +1000

    Update testGame.c

--- testGame.c
+++ testGame.c
@@ -1,6 +1,28 @@
 // test_game.h
 //Created by Micheal, Jospher Lauchlan and Inura
 //For COMP1917 to test whether the game.c of another group will work with our AI
+//Below are the interfaces we will use to make the test steps
+// action codes
+// PASS                 0
+// BUILD_CAMPUS         1
+// BUILD_GO8            2
+// OBTAIN_ARC           3
+// START_SPINOFF        4
+// OBTAIN_PUBLICATION   5
+// OBTAIN_IP_PATENT     6
+// RETRAIN_STUDENTS     7
+ 
+// disciplines
+// STUDENT_THD          0
+// STUDENT_BPS          1
+// STUDENT_BQN          2
+// STUDENT_MJ           3
+// STUDENT_MTV          4
+// STUDENT_MMONEY       5
+//Boolean Operators
+// TRUE                 1
+// FALSE                0
+
 
 Written by Inura De Zoysa
 Game * knowledgeIsland;
@@ -8,9 +30,14 @@ While(assert (knowledgeIsland =! NULL)){
 knowledgeIsland =  newGame (discipline[], dice[]);
 }
 
-action Player;
-//To test make action
+action player;
+player.actionCode = 1;
+player.destination = [L,L,R,R,B];
+player.disciplineFrom;  // used for the retrain students action
+player.disciplineTo;
+assert(isLegalAction (knowledgeIsland, player) == TRUE);
 makeAction (knowledgeIsland, Player);
+assert(isLegalAction (knowledgeIsland, player) == FALSE);
 
 
 disposeGame(knowledgeIsland);
