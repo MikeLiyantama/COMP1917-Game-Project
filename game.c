66722ce4ce3072f651130f512d195527eb10a5f4
IDZ145 <IDZ145@users.noreply.github.com>
2015-05-03 17:26:45 +1000

    Update game.c

--- game.c
+++ game.c
@@ -13,7 +13,40 @@
 #include "Game.h"
 #include "mechanicalTurk.h"
 
-
+typedef struct _game {
+    mostArc
+    mostPub
+    KPI of each player
+    currentTurn
+    whoseTurn
+    currentDice
+    number of Arc
+    number of pubs
+    number of campuses
+    number of g08s
+    number of ips
+    number of students per player
+} Game;
+typedef struct _player {
+    mostArc
+mostPub
+KPI of each player
+currentTurn
+whoseTurn
+currentDice
+number of Arc
+number of pubs
+number of campuses
+number of g08s
+number of ips
+    int THD = 0;
+    int ThD = 0;
+    int BPS = 0;	
+    int B? = 0;
+    int MJ = 0;
+    int MTV = 0;	
+    int M$ = 0;
+}
 
 action decideAction (Game g) {
     
