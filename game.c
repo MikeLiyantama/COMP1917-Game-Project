de7b40c97fce3b1402950eaea0fe2491d29853f2
IDZ145 <IDZ145@users.noreply.github.com>
2015-05-04 22:27:31 +1000

    Update game.c

--- game.c
+++ game.c
@@ -50,3 +50,130 @@ typedef struct _player {
     numberof retrain_M$ = RETRAIN_COST;
 } player;
 
+Game newGame (int discipline[], int dice[]);
+    Game *g = NULL;
+    while ( g == NULL){
+        g = (Game) malloc (sizeof Game);
+    }
+    return g;
+}
+
+void disposeGame (Game g);
+    free (g);
+    assert(g == NULL);
+}
+
+
+void makeAction (Game g, action a);
+
+
+
+
+
+void throwDice (Game g, int diceScore);
+
+
+
+
+
+int getDiscipline (Game g, int regionID);
+
+
+
+
+
+
+int getDiceValue (Game g, int regionID);
+
+
+
+
+
+int getMostARCs(Game g);
+
+
+
+
+int getMostPublications(Game g);
+
+
+
+
+int getTurnNumber (Game g);
+
+
+
+
+
+int getWhoseTurn (Game g);
+
+
+
+
+
+
+int getCampus (Game g, path pathToVortex);
+
+
+
+
+
+int getARC (Game g, path pathToEdge);
+
+
+
+
+
+int isLegalAction (Game g, action a);
+
+
+
+
+
+int getKPIpoints(Game g, int player);
+
+
+
+
+
+int getARCs (Game g, int player);
+
+
+
+
+
+int getGO8s (Game g, int player);
+
+
+
+
+int getCampuses (Game g, int player);
+
+
+
+
+int getIPs (Game g, int player);
+
+
+
+
+
+int getPublications (Game g, int player);
+
+
+
+
+
+int getStudents (Game g, int player, int discipline);
+
+
+
+
+
+
+int getExchangeRate (Game g, int player, int disciplineFrom, int disciplineTo);
+
+
+
+
+
