206ea5a7773ebdcdf0814fa709f136deaf6216e2
IDZ145 <IDZ145@users.noreply.github.com>
2015-05-12 14:01:35 +1000

    Update boardgamemovement.c

--- boardgamemovement.c
+++ boardgamemovement.c
@@ -1,34 +1,39 @@
 // Game board representation using method though up by our group
-
-#define LEFT 1
-#define RIGHT 2
 #define ARCco 'A'
 #define DIVISION 2
 #define NUMBER_OF_DIRECTIONS 6
 typedef int columntype;
+
 typedef struct _board {
-  columntype gameboard [21] [11] = {0};
+  columntype gameboard[21][11];
 } board;
 
-typdef struct co-ordinate {
+typedef struct _co_ordinate {
   int column;
   int row;
   int orientation;
   int valid;
   int isARC;
-} co-ordinate;
+} co_ordinate;
 
-co-ordinate movedecoder (char* directions[],char type) {
+//Prototypes 
+int islegalmovement ( co_ordinate position);
+co_ordinate movedecoder (char* directions,char type);
+co_ordinate movement (co_ordinate position, char route);
+
+
+co_ordinate movedecoder (char* directions,char type) {
    int counter = 0;
-   int route;
-   co-ordinate point = {0,4,0,TRUE,FALSE};
+   char route;
+   co_ordinate point = {0,4,0,TRUE,FALSE};
    if (type == ARCco){
       point.column = 5;
       point.row = 1;
       point.isARC = TRUE;
    }
    while ( (directions[counter] != 0) && (point.valid == TRUE)) {
-      route = direction[counter];
+      route = directions[counter];
+      point = movement(point, route);
       point.valid = islegalmovement (point);
       counter++;      
    }
@@ -36,12 +41,12 @@ co-ordinate movedecoder (char* directions[],char type) {
 }
 
 
-co-ordinate movement (co-ordinate position, int route){
+co_ordinate movement (co_ordinate position, char route){
   unsigned long long direction = (PATH_LIMIT * NUMBER_OF_DIRECTIONS);
   int turning;
-  if (route == RIGHT) {
+  if (route == 'R') {
     turning = -2;
-  } else if ( route == LEFT) {
+  } else if ( route == 'L') {
     turning = 2;
   } else {
     turning = 0;
@@ -69,6 +74,9 @@ co-ordinate movement (co-ordinate position, int route){
     columnMove = 2*columnMove;
   }
   */
+  position.row += rowMove;
+  position.column += columnMove;
+
   position.orientation = ((travel + (NUMBER_OF_DIRECTIONS/2)) % NUMBER_OF_DIRECTIONS); 
   return position;
 }
@@ -76,46 +84,47 @@ co-ordinate movement (co-ordinate position, int route){
 //movement is legal test
 //FOr each row it means that if the number is greater than 
 //the elements in the row or less than zero then the action is illegal
-int islegalmovement ( co-ordinate position) {
+int islegalmovement ( co_ordinate position) {
   int move = FALSE;
-  if((postion.column == 0) || (position.column == 10)){
-    if((position.row >= 5) && (postion.row <= 8) {
+  if((position.column == 0) || (position.column == 10)){
+    if((position.row >= 5) && (postion.row <= 8)) {
     move = TRUE;
     }
   }
   if((postion.column == 1) || (position.column == 9)){
-    if((position.row >= 5 ) && (postion.row <= 8) {
+    if((position.row >= 5 ) && (postion.row <= 8)) {
     move = TRUE;
     }
   }
   if((postion.column == 2) || (position.column == 8)){
-    if((position.row >= 2) && (postion.row < 10) {
+    if((position.row >= 2) && (postion.row < 10)) {
     move = TRUE;
     }
   }
   if((postion.column == 3) || (position.column == 7)){
-    if((position.row >= 2) && (postion.row < 10) {
+    if((position.row >= 2) && (postion.row < 10)) {
     move = TRUE;
     }
   }
   if((postion.column == 4) || (position.column == 8)){
-    if((position.row >= 2) && (postion.row < 10) {
+    if((position.row >= 2) && (postion.row < 10)) {
     move = TRUE;
     }
   }
   if((postion.column == 5) || (position.column == 9)){
-    if((position.row >= 2) && (postion.row < 13) {
+    if((position.row >= 2) && (postion.row < 13)) {
     move = TRUE;
     }
   }
   if((postion.column == 6) || (position.column == 8)){
-    if((position.row > 0) && (postion.row < 13) {
+    if((position.row > 0) && (postion.row <= 13)) {
     move = TRUE;
     }
   }
   if((postion.column == 7)){
-    if((position.row >= 0) && (postion.row >= ) {
+    if((position.row >= 0) && (postion.row <= 13)) {
     move = TRUE;
     }
   }
   return move;
+}
