cf5ecb5b4c0f07348d29d224231265a3b916dd29
lachlan23 <lachocallaghan@gmail.com>
2015-05-12 14:15:45 +1000

    Update newTestGame.c

--- newTestGame.c
+++ newTestGame.c
@@ -122,7 +122,7 @@ int main (int argc, char *argv[]) {
    }
 
    disposeGame(g);        
-   
+   assert(g == NULL);
    printf("All tests passed!\n You are awesome!!\n");
 
    return EXIT_SUCCESS;
